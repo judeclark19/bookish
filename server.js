4c9aa67f885fd37174586be1368ffede00cfa4bb
Jude Clark <judeclark2019@gmail.com>
2020-10-14 10:21:57 -0400

    fullstack starter complete

--- server.js
+++ server.js
@@ -3,22 +3,22 @@ const app = express();
 const exphbs = require("express-handlebars");
 const handlebars = require("handlebars");
 const db = require("./models");
-// const {
-//   allowInsecurePrototypeAccess,
-// } = require("@handlebars/allow-prototype-access");
+const {
+  allowInsecurePrototypeAccess,
+} = require("@handlebars/allow-prototype-access");
 
 const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// app.engine(
-//   "handlebars",
-//   exphbs({
-//     defaultLayout: "main",
-//     handlebars: allowInsecurePrototypeAccess(handlebars),
-//   })
-// );
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main",
+    handlebars: allowInsecurePrototypeAccess(handlebars),
+  })
+);
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
