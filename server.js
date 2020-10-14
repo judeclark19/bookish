4685c7361402943347fa7841ed9327d08bd9972d
Jude Clark <judeclark2019@gmail.com>
2020-10-14 10:02:59 -0400

    abstracted out route files

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
 // db.sequelize.sync().then(function () {
 app.listen(PORT, function () {
   console.log(`Server listening on http://localhost:${PORT}`);
