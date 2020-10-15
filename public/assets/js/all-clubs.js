7d10e80e4797d7a17cd6e7cd404aafea05df00d8
Jude Clark <judeclark2019@gmail.com>
2020-10-15 15:17:35 -0400

    cleaning up my git shit mess

---
+++ public/assets/js/all-clubs.js
@@ -0,0 +1,117 @@
+// Dummy data
+// ===============================================================================================================
+console.log("ALL CLUBS JS IS RUNNING");
+const allClubs = [
+  {
+    id: 1,
+    name: "Outdoorsy Club",
+    currentBookTitle: "Call of the Wild",
+    progress: 25,
+    members: 8,
+    active: true,
+  },
+  {
+    id: 2,
+    name: "Tiny Fey Lovers",
+    currentBookTitle: "Bossypants",
+    progress: 42,
+    members: 6,
+    active: true,
+  },
+  {
+    id: 3,
+    name: "Scotland Yard",
+    currentBookTitle: "The Sign of Four",
+    progress: 66,
+    members: 10,
+    active: true,
+  },
+  {
+    id: 4,
+    name: "Romance Novels Club",
+    currentBookTitle: "Troubles in Paradise",
+    progress: 6,
+    members: 5,
+    active: true,
+  },
+  {
+    id: 5,
+    name: "Self Help Club",
+    currentBookTitle: "Self Help Book",
+    progress: 41,
+    members: 10,
+    active: false,
+  },
+  {
+    id: 6,
+    name: "Science Fiction",
+    currentBookTitle: "Dune",
+    progress: 61,
+    members: 10,
+    active: true,
+  },
+  {
+    id: 7,
+    name: "Comedy Club",
+    currentBookTitle: "Me Talk Pretty One Day",
+    progress: 88,
+    members: 7,
+    active: true,
+  },
+  {
+    id: 8,
+    name: "History Club",
+    currentBookTitle: "Hamilton",
+    progress: 100,
+    members: 8,
+    active: false,
+  },
+];
+
+$(document).ready(function () {
+  //   var br = document.createElement("br");
+
+  allClubs.forEach((club) => {
+    if (club.active) {
+      const clubCard = $("<div class='club-card columns'>");
+      $(".body-mat").append(clubCard);
+
+      const bookInfoDiv = $("<div class='column is-one-fourth book-info-div'>");
+      const bookCoverImg = $("<img class='book-cover'>").attr(
+        "src",
+        "https://via.placeholder.com/450x720.png"
+      );
+      const bookDataTemp = $("<p>").text(
+        "Title, author, genre, year etc will go here"
+      );
+      bookInfoDiv.append(bookCoverImg, bookDataTemp);
+
+      const clubInfoDiv = $("<div class='column club-info-div'>");
+      const clubName = $("<p class='club-name'>").text(club.name);
+      const progressBarLabel = $("<p class='progress-bar-label'>").text(
+        "Progress bar label idk"
+      );
+      const progressBar = $("<progress class='progress' max='100'>").attr(
+        "value",
+        club.progress
+      );
+      clubInfoDiv.append(clubName, progressBarLabel, progressBar);
+
+      const joinClubDiv = $("<div class='column is-one-fifth join-club-div'>");
+      const joinClubButton = $("<button class='button'>").text("Join Club");
+      const clubFullMsg = $("<p class='club-full-msg'>").text("Club FULL");
+      const clubInactiveMsg = $("<p class='club-inactive-msg'>").text(
+        "This club is inactive"
+      );
+      if (club.active && club.members < 10) {
+        joinClubDiv.append(joinClubButton);
+      } else if (club.active && club.members >= 10) {
+        joinClubDiv.append(clubFullMsg);
+      } else {
+        joinClubDiv.append(clubInactiveMsg);
+      }
+
+      clubCard.append(bookInfoDiv, clubInfoDiv, joinClubDiv);
+    }
+  });
+});
