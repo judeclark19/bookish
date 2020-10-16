e8c39ecd5229627121a687b05d02cedcb6c83b9a
AK <icecicle04@gmail.com>
2020-10-16 11:05:26 -0400

    updated routes and axxios call

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,6 +2,7 @@
 var db = require("../models");
 const axios = require("axios");
 const bcrypt = require("bcryptjs");
+const { post } = require("jquery");
 require("dotenv").config();
 // var passport = require("../config/passport");
 
@@ -21,16 +22,17 @@ module.exports = function (app) {
   // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
 
-  app.post("/api/searchByAuthor", function (req, res) {
+  app.post("/api/search-results", function (req, res) {
     console.log(req.body);
+    res.json(req.body);
     axios({
       method: "get",
-      url: `https://v1.nocodeapi.com/icecicle04/gr/${process.env.GOODREADS_KEY}/searchAuthor`,
+      url: `https://v1.nocodeapi.com/alikhan/gr/${process.env.GOODREADS_KEY}/searchAuthor?q=${newSearchValue}`,
       params: { q: "<q>" },
     })
       .then(function (response) {
         // handle success
-        console.log(response.data);
+        post(response.data);
       })
       .catch(function (error) {
         // handle error
@@ -38,16 +40,18 @@ module.exports = function (app) {
       });
   });
 
-  app.post("/api/searchByBook", function (req, res) {
+  app.post("/api/search-results", function (req, res) {
     console.log(req.body);
+    res.json(req.body);
     axios({
       method: "get",
-      url: `https://v1.nocodeapi.com/icecicle04/gr/${process.env.GOODREADS_KEY}/search`,
+      url: `https://v1.nocodeapi.com/alikhan/gr/${process.env.GOODREADS_KEY}/search?q=${newSearchValue}`,
       params: { q: "<q>" },
     })
       .then(function (response) {
         // handle success
-        console.log(response.data);
+        post(response.data);
+        console.log(res.data);
       })
       .catch(function (error) {
         // handle error
@@ -73,28 +77,27 @@ module.exports = function (app) {
   // route to log users in
   app.post("/api/login", function (req, res) {
     console.log(req.body);
-    db.User.findOne({ where:
-      {email: req.body.email}
-    }).then(function (foundUser) {
-      // console.log(foundUser);
-      // compare the saved/hashed password with the password put in on the page 
-     bcrypt.compare(req.body.password, foundUser.password).then(function(result){
-     
-       if (foundUser.email === req.body.email && result === true) {
-        // create a route for logged in users to be sent to (what happens next?)
-        // think through which routes should be accessible for the users -- 'My account' page?
-        console.log(req.session)
-        res.render("my-account", {email: req.body.email});
-        console.log("Succesfully logged in user!");
-      } else {
-        res.redirect("/api/login");
-        console.log("Invalid email or password");
-      }
-     })
-      
-    }).catch((err) =>{
-      if(err) throw err;
-    
+    db.User.findOne({ where: { email: req.body.email } })
+      .then(function (foundUser) {
+        // console.log(foundUser);
+        // compare the saved/hashed password with the password put in on the page
+        bcrypt
+          .compare(req.body.password, foundUser.password)
+          .then(function (result) {
+            if (foundUser.email === req.body.email && result === true) {
+              // create a route for logged in users to be sent to (what happens next?)
+              // think through which routes should be accessible for the users -- 'My account' page?
+              console.log(req.session);
+              res.render("my-account", { email: req.body.email });
+              console.log("Succesfully logged in user!");
+            } else {
+              res.redirect("/api/login");
+              console.log("Invalid email or password");
+            }
+          });
+      })
+      .catch((err) => {
+        if (err) throw err;
       });
   });
 
