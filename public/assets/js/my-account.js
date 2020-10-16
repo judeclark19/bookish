0007397636d7822fad5bc37bd3dae8649e52ba24
Jude Clark <judeclark2019@gmail.com>
2020-10-16 11:21:23 -0400

    inputs and buttons both toggle on the my account page

--- public/assets/js/my-account.js
+++ public/assets/js/my-account.js
@@ -1,14 +1,43 @@
 $(function () {
   console.log("my acccount.js ready");
   $("#edit-username-btn").click(function () {
-    console.log("Clicked edit username button");
+    $("#edit-username-btn").addClass("hidden");
+    $("#save-username-btn").removeClass("hidden");
+    $("#username-span").addClass("hidden");
+    $("#edit-username-input").removeClass("hidden");
+  });
+
+  $("#save-username-btn").click(function () {
+    $("#save-username-btn").addClass("hidden");
+    $("#edit-username-btn").removeClass("hidden");
+    $("#username-span").removeClass("hidden");
+    $("#edit-username-input").addClass("hidden");
   });
 
   $("#edit-email-btn").click(function () {
-    console.log("Clicked edit email button");
+    $("#edit-email-btn").addClass("hidden");
+    $("#save-email-btn").removeClass("hidden");
+    $("#email-span").addClass("hidden");
+    $("#edit-email-input").removeClass("hidden");
   });
 
+  $("#save-email-btn").click(function () {
+    $("#save-email-btn").addClass("hidden");
+    $("#edit-email-btn").removeClass("hidden");
+    $("#email-span").removeClass("hidden");
+    $("#edit-email-input").addClass("hidden");
+  });
   $("#edit-pw-btn").click(function () {
-    console.log("Clicked edit password button");
+    $("#edit-pw-btn").addClass("hidden");
+    $("#save-pw-btn").removeClass("hidden");
+    $("#pw-span").addClass("hidden");
+    $("#edit-pw-input").removeClass("hidden");
+  });
+
+  $("#save-pw-btn").click(function () {
+    $("#save-pw-btn").addClass("hidden");
+    $("#edit-pw-btn").removeClass("hidden");
+    $("#pw-span").removeClass("hidden");
+    $("#edit-pw-input").addClass("hidden");
   });
 });
