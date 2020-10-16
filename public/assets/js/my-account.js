c003fa1a947379e7923f83695e5d3a7289402df4
Jude Clark <judeclark2019@gmail.com>
2020-10-16 10:35:47 -0400

    added edit and save buttons to my account page

---
+++ public/assets/js/my-account.js
@@ -0,0 +1,14 @@
+$(function () {
+  console.log("my acccount.js ready");
+  $("#edit-username-btn").click(function () {
+    console.log("Clicked edit username button");
+  });
+
+  $("#edit-email-btn").click(function () {
+    console.log("Clicked edit email button");
+  });
+
+  $("#edit-pw-btn").click(function () {
+    console.log("Clicked edit password button");
+  });
+});
