e09ae102879bea11d3093f2bd5e0e2c607890a24
AK <icecicle04@gmail.com>
2020-10-17 23:46:46 -0400

    added book model

--- models/club.js
+++ models/club.js
@@ -1,36 +1,56 @@
-
-// generate a Club model for the db 
+// generate a Club model for the db
 module.exports = function (sequelize, DataTypes) {
-    const Club = sequelize.define("Club", {
-        club_name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-            validate: {
-                len: [2, 35],
-            }
-        },
+  const Club = sequelize.define("Club", {
+    club_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [2, 35],
+      },
+    },
 
-        book_name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-            validate: {
-                len: [2, 35],
-            }
-        }
-        // userId: DataTypes.Integer
+    book_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [2, 35],
+      },
+    },
+    // userId: DataTypes.Integer
+  });
 
-
+  Club.associate = function (models) {
+    // Associating Author with Posts
+    // When an Author is deleted, also delete any associated Posts
+    Club.hasMany(models.User, {
+      onDelete: "cascade",
     });
+  };
 
-    Club.associate = function (models) {
-        // Associating Author with Posts
-        // When an Author is deleted, also delete any associated Posts
-        Club.hasMany(models.User, {
-            onDelete: "cascade"
-        });
-    }
+  return Club;
+};
 
-    return Club;
-}
+// // generate book model for the db
+
+// module.exports = function (sequelize, DataTypes) {
+//     const Book = sequelize.define("Book", {
+//         book_names: {
+//             type: DataTypes.STRING,
+//             allowNull: false,
+//             unique: true,
+//             validate: {
+//                 len: [2, 35],
+//             }
+//         },
+
+//         book_id: {
+//             type: DataTypes.STRING,
+//             allowNull: false,
+//             unique: true,
+//             validate: {
+//                 len: [2, 35],
+//             }
+//         }
+//         // userId: DataTypes.Integer
