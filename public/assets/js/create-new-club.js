129f2b0c75c2fa89542673ab657e211489f535c3
AK <icecicle04@gmail.com>
2020-10-18 01:23:30 -0400

    restarting this

--- public/assets/js/create-new-club.js
+++ public/assets/js/create-new-club.js
@@ -2,13 +2,19 @@ $(function () {
   // connect with the create club form
   $(".create-club-by-book").on("click", function (event) {
     event.preventDefault();
-    {
-      let newClubName = $(
-        "<p>What would you like to name this club?</p>"
-      ).prompt(() => {});
-
+    let newClubName = $(
+      “<p>What would you like to name this club?</p>”
+    ).prompt(() => {});
+    console.log("Inputs:");
+    console.log($("#new-club-input").val());
+    console.log($("#book-name-input").val());
+    if (!$("#new-club-input").val() || !$("#book-name-input").val()) {
+      alert("Please enter valid information");
+      return;
+      // create a newClub variable to store the information
+    } else {
       newClub = {
-        club_name: $(newClubName).val().trim(),
+        club_name: $("#new-club-input").val().trim(),
         book_name: $("#book-name-input").val().trim(),
       };
       // console.log(newClub);
