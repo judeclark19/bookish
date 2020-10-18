eca80a072fdebca5ab2de7adac6e6b249bbbf691
Cody Bonsma <codybonsma@gmail.com>
2020-10-18 10:20:55 -0400

    commit before push

--- models/club.js
+++ models/club.js
@@ -25,14 +25,14 @@ module.exports = function (sequelize, DataTypes) {
     },
   });
 
-  // Club.associate = function (models) {
-  //   // Associating user with clubs
-  //   // When a user is deleted, also delete any associated clubs
-  //   // TODO: I don't think we need to cascade delete these
-  //   Club.hasMany(models.User, {
-  //     // onDelete: "cascade",
-  //   });
-  // };
+//   Club.associate = function (models) {
+//     // Associating user with clubs
+//     // When a user is deleted, also delete any associated clubs
+//     // TODO: I don't think we need to cascade delete these
+//     Club.hasMany(models.User, {
+//       // onDelete: "cascade",
+//     });
+//   };
 
   return Club;
 };
